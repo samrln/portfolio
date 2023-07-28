8989de326f164ef1f56f76806d9fab67e432a672
Samantha Nogueira <122844984+samrln@users.noreply.github.com>
2023-07-27 23:45:23 -0300

    add portfolio engineer (#1)
    * add portfolio engineer
    
    * fix: layout page
    
    * remove image
    
    ---------
    
    Co-authored-by: Samantha Nogueira <samanthanogueira@MacBook-Air-de-Samantha.local>

--- src/pages/EngineerPortfolio.vue
+++ src/pages/EngineerPortfolio.vue
@@ -1,5 +1,54 @@
 <template>
-    <div class="EngineerPortfolio">
-        <a> ol[a] EngineerPortfolio</a>
+  <div class="engineer">
+    <div class="header">
+      <h1>Portfólio de Engenharia</h1>
     </div>
+    <div class="university-information">
+        <h2>Formação acadêmica</h2>
+        <h3>Engenharia de Energia</h3>
+        <h4>UNESP - Universidade Estadual Paulista "Júlio de Mesquita Filho"</h4>
+        <p>Feveriro de 2017 - Agosto de 2022</p>
+    </div>
+    <div class="formation">
+      <div class="resumos">
+        <div>
+          <h2>Empresa Júnior</h2>
+          <h3>FONTES</h3>
+          <p>
+            Durante minha participação na empresa júnior Fontes, atuei no cargo de Projetos, desempenhando um papel
+            fundamental no desenvolvimento de projetos voltados para a área de engenharia de energia. Contribuí para a
+            criação de soluções inovadoras, aplicando meus conhecimentos técnicos e habilidades de gerenciamento de
+            projetos. Minha experiência na Fontes me proporcionou uma visão abrangente do setor e a oportunidade de
+            aplicar meu aprendizado acadêmico em desafios reais.
+          </p>
+        </div>
+        <div>
+          <h2>Iniciação Científica</h2>
+          <h3>Método de Diferenças Finitas Aplicadas à Lei de Resfriamento de Newton</h3>
+          <p>
+            Estudo dos métodos numéricos para a resolução de equações diferenciais, em
+            particular, a equação de distribuição da temperatura em módulos fotovoltaicos.
+          </p>
+        </div>
+        <div>
+          <h2>TCC</h2>
+          <h3>Sistemas Scada as a Service (SaaS) para gestão de energia comparados aos sistemas convencionais</h3>
+          <p>
+            SCADA as a Service é um sistema remoto de controle e aquisição de dados em nuvem que permite monitorar e
+            processar informações, registrando eventos. Um estudo sobre o uso desse sistema na gestão de energia busca
+            compreender suas vantagens, desvantagens e diferenças em relação aos sistemas tradicionais. A possibilidade de
+            hospedar o SCADA na nuvem cria um novo modelo de negócio com benefícios significativos para o gerenciamento
+            energético.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="certificate">
+      <p>
+        <a href="www.google.com.br">Certificados</a>
+      </p>
+    </div>
+  </div>
 </template>
+
+<style src="../styles/engineerPortfolio.css" scoped></style>
